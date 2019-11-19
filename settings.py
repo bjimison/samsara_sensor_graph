51f68f415312c25b869bd3cb13a7f86a145e93df
bjimison <brady.t.jimison@gmail.com>
2019-11-18 16:39:17 -0800

    added necessary access and ID variables into a class Constants to be imported into main.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Constants():
+    group_id = "GROUP_ID_HERE" #found in the URL of your samsara dashboard homepage
+    access_token = "ACCESS_TOKEN_HERE"
+    temp_widget_id = "TEMP_WIDGET_ID_HERE"
+    temperature_end_point = "https://api.samsara.com/v1/sensors/temperature"
+    api_time_format = "%Y-%m-%dT%H:%M:%SZ"
