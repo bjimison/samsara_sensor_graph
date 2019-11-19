b4bcfdf421a735b3a47c4068cde276e086ec3c66
bjimison <brady.t.jimison@gmail.com>
2019-11-18 18:06:12 -0800

    imported sensor object, graph object, and Constants from supporting files to run the program from main.py, with given interval, length, and data points to place in graph object with the begin_plotting class method invoked

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ from settings import Constants
 from sensor_graph import SensorGraph
 from temp_sensor import TempSensor
 
+def main():
+    # create the temp sensor object with x,y values 
+    temperature_sensor = TempSensor(Constants.group_id, Constants.temp_widget_id)
+
+    # graph object which takes the object above and plots it at interval
+    interval = 3 * 60
+    graph_points = 10 #amount plotted at any given instance
+    sensor_graph = SensorGraph(interval, graph_points, temperature_sensor)
+
+    sensor_graph.begin_plotting()
+
+if __name__ == "__main__":
+    main()
